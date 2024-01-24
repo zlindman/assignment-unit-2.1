27fdf3ef17ffd75ebd79682e619eae4ec206e686
Zoe Lindman <zoe.lindman@gmail.com>
2024-01-23 19:45:31 -0600

    completed questions 1-14

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,48 +1,77 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
-let firstName = '';
+let firstName = 'Zoe';
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
-
+let lastName = ' Lindman';
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
+let fullName = firstName + lastName;
 //     (remember, you already have variables for this, can you use those?)
 //     Console log the value of `fullName`
-
+console.log (fullName);
 // 4 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
 //     Console log the value of `luckyNumber`
+let luckyNumber = 16;
 
 // 5 - Create a variable `introduction` and using the variables from above,
+let introduction = "My name is " + fullName + ", and I think " + luckyNumber + " is a winner!";
 //     give it the value of:
 //     'My name is (full name), and I think (lucky number) is a winner!'.
 //     Refer back to the videos if you need help with this one.
 //     Console log the value of `introduction`
+console.log (introduction);
 
 // 6 - Create a variable named `adventurous` and set it to a boolean value (true or false)
+let adventurous = true;
 //     Console log the value of `adventurous`
-
+console.log (adventurous);
 // 7 - Create a variable named `food`, and set its value to a string of your favorite food
+let food = 'sushi';
 //     Console log the value of `food`
-
+console.log (food);
 // 8 - Create a variable called `pets` and set it to the value of the number of pets you have
+let pets = 3
 //     Console log the value of `pets`
+console.log (pets)
 
 // 9 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
-
+let friendsPets = 2
 // 10 - Add two pets to your `pets` variable
-
+pets = pets + 2
 // 11 - Create a constant variable called `allowedPets` and set it to a number value of your choice
-
+const allowedPets = 3
 // 12 - Create a variable called `result`. Create a conditional:
+let result = "Adventures are great!";
+if (adventurous = true) {
+  "Adventures are great!";
+}
+else {
+  "How about we stay home?";
+}
 //      if adventurous is true, set `result` to be "Adventures are great!", 
 //      if it's not true,  set `result` to be "How about we stay home?"
 //      Console log the value of `result`
-
+console.log (result);
 // 13 - Create a variable called `diceRoll` and set it to the value of "Try again later.".
+let diceRoll = "Try again later.";
+if (luckyNumber == 2 && adventurous == true){
+diceRoll = "Roll the dice!";
+}
+console.log (diceRoll);
 //      Create a compound conditional: 
 //      if luckyNumber is 2 and adventurous is true,
 //      set `diceRoll` to be "Roll the dice!"
 //      Console log the value of `diceRoll`
 
 // 14 - Create a variable called `petStatus`. 
+let petStatus = "I can have more pets";
+if (pets < allowedPets){
+   petStatus = "I can have more pets."};
+if (pets = allowedPets){
+  petStatus = "I have enough pets."};
+if (pets > allowedPets) {
+  petStatus = "Oh no, I have too many pets!";
+}
+console.log (petStatus);
 //      Write a conditional that covers the following:
 //      if the value of `pets` is less than the value of `allowedPets`,
 //      set `petStatus` to the value of "I can have more pets" 
@@ -95,7 +124,7 @@ try {
     pets: typeof pets !== 'undefined' ? pets : undefined,
     friendsPets: typeof friendsPets !== 'undefined' ? friendsPets : undefined,
     allowedPets: typeof allowedPets !== 'undefined' ? allowedPets : undefined,
-    result: typeof result !== 'undefined' ? result : undefined,
+    result: typeof adventurous !== 'undefined' ? adventurous : undefined,
     diceRoll: typeof diceRoll !== 'undefined' ? diceRoll : undefined,
     petStatus: typeof petStatus !== 'undefined' ? petStatus : undefined,
     mostPets: typeof mostPets !== 'undefined' ? mostPets : undefined,
